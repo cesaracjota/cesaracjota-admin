25a239871e1e34de949d940adca8ec4c234ce6cf
cesaracjota <cesar.acjota@tecsup.edu.pe>
2023-12-06 06:33:50 -0500

    solution error 404

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,6 @@ const env = loadEnv(process.env.MODE, process.cwd(), '');
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  rollupOptions: {
-    input: {
-      main: resolve(__dirname, 'index.html'),
-      nested: resolve(__dirname, 'nested/index.html'),
-    },
-  },
   plugins: [react()],
   build: {
     target: 'esnext',
